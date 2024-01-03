8f33ffd832872cfc485431e6a013b8ef761e933f
BasilZap <zapr9@yandex.ru>
2024-01-03 04:53:06 +0300

    Созданы представления для моделей "Продукты", "Завод", "Поставщик", "Предприниматель", прописана маршрутизация в urls

--- electronet/views.py
+++ electronet/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from electronet.models import Product, Factory, Distributor, Businessman
+from electronet.permissions import IsActive
+from electronet.serializers import ProductSerializer, FactorySerializer, DistributorSerializer, BusinessmanSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    """
+    Вьюсет для модели "Продукт"
+    """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    action_permissions = [IsActive]
+
+
+class FactoryViewSet(viewsets.ModelViewSet):
+    """
+    Вьюсет для модели "Завод"
+    """
+    queryset = Factory.objects.all()
+    serializer_class = FactorySerializer
+    action_permissions = [IsActive]
+
+
+class DistributorViewSet(viewsets.ModelViewSet):
+    """
+    Вьюсет для модели "Поставщик"
+    """
+    queryset = Distributor.objects.select_related('distributor')
+    serializer_class = DistributorSerializer
+    action_permissions = [IsActive]
+
+
+class BusinessmanViewSet(viewsets.ModelViewSet):
+    """
+    Вьюсет для модели "Предприниматель"
+    """
+    queryset = Businessman.objects.select_related('distributor', 'factory')
+    serializer_class = BusinessmanSerializer
+    action_permissions = [IsActive]
